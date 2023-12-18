ca3fb00fcaef5d30cf4697847a64e6fdf1deb70d
Caleb Tetteh <calebtetteh65@gmail.com>
2023-12-18 09:23:42 +0000

    made some changes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+app.post('/patient', (req, res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
 
 mongoose.connect('mongodb+srv://admin:1111@databaseapi.ijixrhd.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(() => {
